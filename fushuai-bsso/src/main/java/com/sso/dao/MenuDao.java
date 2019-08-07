0c4990002ea682a5074a93daa917bee0da786cbd
xiaiyu <1141600195@qq.com>
2019-08-07 10:40:46 +0800

    添加目录

---
+++ fushuai-bsso/src/main/java/com/sso/dao/MenuDao.java
@@ -0,0 +1,16 @@
+package com.sso.dao;
+
+import com.kh.pojo.entity.MenuInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+/**
+ * 获得目录
+ */
+public interface MenuDao extends JpaRepository<MenuInfo, Long> {
+
+    @Query(value = "select bm.* from base_role_menu brm inner join base_menu bm on brm.menuId=bm.id where brm.roleId=?1 and bm.parentId=?2  ", nativeQuery = true)
+    public List<MenuInfo> getMenuInfo(Long roleId, Long parentId);
+}
